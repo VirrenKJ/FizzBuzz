f9690a1bc5144b608c1b816ebcd1fb60947a2714
VirrenKJ <virendra.kjarwal.cer12@iitbhu.ac.in>
2020-09-24 14:02:06 +0530

    Testing for Fizz when no. is 3 is unsuccessful.

---
+++ src/com/kata/FizzBuzz.java
@@ -0,0 +1,12 @@
+package com.kata;
+
+
+public class FizzBuzz {
+
+    public String play(int number){
+        return String.valueOf(number);
+    }
+
+    public static void main(String[] args) {
+    }
+}
---
+++ src/com/kata/FizzBuzzTest.java
@@ -0,0 +1,22 @@
+package com.kata;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class FizzBuzzTest {
+
+    private FizzBuzz fizzBuzz;
+
+    @Before
+    public void setup(){
+        fizzBuzz = new FizzBuzz();
+    }
+
+    @Test
+    public void testFizz(){
+        int number = 3;
+        String numberReturned = fizzBuzz.play(number);
+        Assert.assertEquals("Fizz", numberReturned);
+    }
+}
