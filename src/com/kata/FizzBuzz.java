fafac8102a1d373f520b08a79bda8502f57e3950
VirrenKJ <virendra.kjarwal.cer12@iitbhu.ac.in>
2020-09-24 15:52:48 +0530

    Test failed for a number divisible by 3 or if it has 3 in it.

--- src/com/kata/FizzBuzz.java
+++ src/com/kata/FizzBuzz.java
@@ -7,7 +7,7 @@ public class FizzBuzz {
         if(number%15 == 0){
             return "FizzBuzz";
         }
-        if(number%3 == 0){
+        if(number%3 == 0 || String.valueOf(number).contains("3")){
             return "Fizz";
         }
         if(number%5 == 0){
