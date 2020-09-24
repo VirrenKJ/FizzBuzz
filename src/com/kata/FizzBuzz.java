5149a8ff31e0f3576ed01608a61f922d951136b7
VirrenKJ <virendra.kjarwal.cer12@iitbhu.ac.in>
2020-09-24 14:17:39 +0530

    Testing for Buzz when no. is divisible by both 3 and 5 passed.

--- src/com/kata/FizzBuzz.java
+++ src/com/kata/FizzBuzz.java
@@ -4,6 +4,9 @@ package com.kata;
 public class FizzBuzz {
 
     public String play(int number){
+        if(number%15 == 0){
+            return "FizzBuzz";
+        }
         if(number%3 == 0){
             return "Fizz";
         }
